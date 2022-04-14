d01cbd4a05b260d6209d792112500d9dfe315a2f
asbljy <asbljy@outlook.com>
2022-04-14 00:35:42 -0400

    update

--- data/sec2sentence.py
+++ data/sec2sentence.py
@@ -13,6 +13,8 @@ from lxml import etree              # 导入库
 from bs4 import BeautifulSoup
 import re
 import json
+from nltk.translate.bleu_score import sentence_bleu, SmoothingFunction
+smooth = SmoothingFunction()
 from rank_bm25 import BM25Okapi
 def is_english(key):
     return key.encode('UTF-8').isalpha()
@@ -30,21 +32,26 @@ complete_corpus = []
 group_list = []
 sen2id = {}
 for fid, file in tqdm(enumerate(my_data)):
-    sentences = ('。'.join(file['rsecs'])).split('。')
-    id_list = []
     sen_set = set()
-    for sentence in sentences:
-        if len(sentence) <= 3:
-            continue
-        if sentence in sen_set:
-            continue
-        else:
-            sen_set.add(sentence)
-        if sentence in sen2id.keys():
-            id_list.append(sen2id[sentence])
-        else:
-            sen2id[sentence] = len(sen2id)
-            id_list.append(sen2id[sentence])
+    id_list = []
+    if len(''.join(file['rsecs']))==0 and len(file['rpsecs'])>0:
+        file['rsecs'] = file['rpsecs'][0]
+    for example in file['rsecs']:
+        exp_id_list = []
+        sentences = example.split('。')
+        for sentence in sentences:
+            if len(sentence) <= 3:
+                continue
+            if sentence in sen_set:
+                continue
+            else:
+                sen_set.add(sentence)
+            if sentence in sen2id.keys():
+                exp_id_list.append(sen2id[sentence])
+            else:
+                sen2id[sentence] = len(sen2id)
+                exp_id_list.append(sen2id[sentence])
+        id_list.append(exp_id_list)
     group_list.append(id_list)
 complete_corpus = []
 for sen in tqdm(sen2id.keys()):
@@ -52,30 +59,38 @@ for sen in tqdm(sen2id.keys()):
     complete_corpus.append(tokenized_corpus)
 bm25_sentences = BM25Okapi(complete_corpus)
 for fid, (file, index_list) in tqdm(enumerate(zip(my_data, group_list))):
-    if len(index_list) <=0:
+    if len(index_list) <= 0:
         continue
     # print(file)
     key = file['key']
     anno = file['anno']
     key_anno = key + ' ' +anno
     key_cut = jieba.lcut(anno)
-    batch_scores = bm25_sentences.get_batch_scores(key_cut, index_list)
-    top_n_sentence = ''.join(complete_corpus[index_list[np.argmax(batch_scores)]])
-    print(anno)
-    print(top_n_sentence)
-    if max(batch_scores) <= 2 and key not in top_n_sentence:
-        if anno in key:
-            key = anno
-            file['key'] = key
-
-        else:
-            count_f += 1
-            print(key)
-            print(anno)
-            print(top_n_sentence)
-            print('--------------')
+    flag = True
+    top_n_sentences = []
+    for exp_id_list in index_list:
+        if len(exp_id_list) == 0:
             continue
-    file['rsecs'] = [top_n_sentence]
+        batch_scores = bm25_sentences.get_batch_scores(key_cut, exp_id_list)
+        top_n_sentence = ''.join(complete_corpus[exp_id_list[np.argmax(batch_scores)]])
+        can_simi = sentence_bleu([complete_corpus[exp_id_list[np.argmax(batch_scores)]]], key_cut, weights=(0.5, 0.5),
+                                 smoothing_function=smooth.method1)
+        if max(batch_scores) <= 2 and key not in top_n_sentence and can_simi < 0.5:
+            if anno in key:
+                key = anno
+                file['key'] = key
+                flag = False
+        else:
+            flag = False
+        top_n_sentences.append(top_n_sentence)
+    if flag:
+        count_f += 1
+        print(key)
+        print(anno)
+        print(top_n_sentences)
+        print('--------------')
+        continue
+    file['rsecs'] = top_n_sentences
     sentences = []
     for gid, group in enumerate(file['rpsecs']):
         sentences = group[0:-1]
