1a267edbb7f5182dfd40fef3dcb7ff1fee63374a
asbljy <asbljy@outlook.com>
2022-04-28 01:14:51 -0400

    update

--- train_full.py
+++ train_full.py
@@ -62,7 +62,7 @@ def build(config):
     models.named_parameters()
     optimizer_p = AdamW(modelp.parameters(), lr=config.lr)
     optimizer_s = AdamW(models.parameters(), lr=config.lr)
-    optimizer_decoder = AdamW(optimizer_grouped_parameters+[x for x in modelp.parameters()]+[x for x in models.parameters()], lr=config.lr * 0.1)
+    optimizer_decoder = AdamW(optimizer_grouped_parameters, lr=config.lr * 0.1)
     loss_func = torch.nn.CrossEntropyLoss()
     return modelp, models, model, optimizer_p, optimizer_s, optimizer_decoder, train_dataloader, valid_dataloader, test_dataloader, loss_func, titles, sections, title2sections, sec2id, bm25_title, bm25_section, tokenizer
 
