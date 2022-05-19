2f88b2bf1ed6cac8dc05ceafca0035eeebcf386d
asbljy <asbljy@outlook.com>
2022-05-19 15:23:24 -0400

    update

--- train_full.py
+++ train_full.py
@@ -181,10 +181,10 @@ def train_eval(modelp, models, modele, modeld, optimizer_p, optimizer_s, optimiz
                 outputs_annotation = modele(input_ids=reference_ids, attention_adjust=adj_matrix)
             hidden_annotation = outputs_annotation.decoder_hidden_states[:, 1:config.hidden_anno_len+1]
             if modeld_p is not None:
-                outputs = modeld_p(input_ids=decoder_ids, decoder_input_ids=target_ids_for_train, cut_indicator=cut_list,
+                outputs = modeld_p(input_ids=decoder_ids, decoder_input_ids=target_ids, cut_indicator=cut_list,
                                  anno_position=decoder_anno_position, hidden_annotation=hidden_annotation)
             else:
-                outputs = modeld(input_ids=decoder_ids, decoder_input_ids=target_ids_for_train, cut_indicator=cut_list, anno_position=decoder_anno_position, hidden_annotation=hidden_annotation)
+                outputs = modeld(input_ids=decoder_ids, decoder_input_ids=target_ids, cut_indicator=cut_list, anno_position=decoder_anno_position, hidden_annotation=hidden_annotation)
             logits_ = outputs.logits
             len_anno = min(target_ids.shape[1], logits_.shape[1])
             logits = logits_[:, 0:len_anno-1]
