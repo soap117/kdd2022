d69da8f8f5e92d7f9ac0acf9ea65d6452eacacc3
asbljy <asbljy@outlook.com>
2022-05-24 20:53:08 -0400

    update

---
+++ eval/check.py
@@ -0,0 +1,86 @@
+import pickle
+
+import numpy as np
+from transformers import BertTokenizer
+from nltk.translate.bleu_score import sentence_bleu
+import nltk
+from datasets import load_metric
+bert_model = 'fnlp/bart-base-chinese'
+tokenizer = BertTokenizer.from_pretrained(bert_model)
+def obtain_annotation(src, tar):
+    t_s = 0
+    annotations = []
+    while t_s < len(tar):
+        if tar[t_s] == '（':
+            t_e = t_s + 1
+            while t_e < len(tar) and tar[t_e] != '）' and tar[t_e] !='（':
+                t_e += 1
+            if t_e == len(tar):
+                break
+            anno_posi = tar[t_s+1:t_e]
+            if len(anno_posi) > 0 and anno_posi not in src and tar[t_e] == '）':
+                annotations.append(anno_posi)
+            t_s = t_e
+        else:
+            t_s += 1
+    return annotations
+
+def get_hit_score(srcs, tars, pres):
+    recalls = []
+    precisions = []
+    for src, tar, pre in zip(srcs, tars, pres):
+        annotations = obtain_annotation(src, tar)
+        annotations_pre = obtain_annotation(src, pre)
+        common_ones = 0
+        for annotation_pre in annotations_pre:
+            for annotation in annotations:
+                bleu = get_sentence_bleu(tokenizer.tokenize(annotation_pre), [tokenizer.tokenize(annotation)])
+                if bleu > 0.5:
+                    common_ones += 1
+                    break
+        total = len(annotations_pre)+len(annotations)-common_ones
+        score = common_ones/(len(annotations)+1e-9)
+        recalls.append(score)
+        score = common_ones / (len(annotations_pre) + 1e-9)
+        precisions.append(score)
+    return {'anno_recall': np.mean(recalls), 'anno_precision': np.mean(precisions)}
+
+def get_sentence_bleu(candidate, reference):
+    score = sentence_bleu(reference, candidate)
+    return score
+def count_bleu_score(candidate, reference):
+    avg_score = 0
+    for k in range(len(candidate)):
+        reference_ = reference[k]
+        for m in range(len(reference_)):
+            reference_[m] = tokenizer.tokenize(reference_[m])
+        candidate[k] = tokenizer.tokenize(candidate[k])
+        try:
+            avg_score += get_sentence_bleu(candidate[k], reference_)/len(candidate)
+        except:
+            print(candidate[k])
+            print(reference[k])
+    return avg_score
+results = pickle.load(open('../data/test/my_results_sec.pkl', 'rb'))
+hit_score = get_hit_score(results['srcs'], results['tars'], results['prds'])
+print(hit_score)
+outs = [' '.join(tokenizer.tokenize(u)) for u in results['prds']]
+ints = [' '.join(tokenizer.tokenize(u)) for u in results['srcs']]
+refs = [[' '.join(tokenizer.tokenize(u))] for u in results['tars']]
+sari = load_metric("sari")
+bleu = load_metric("bleu")
+meteor = load_metric('meteor')
+sari_score = sari.compute(sources=ints, predictions=outs, references=refs)
+print(sari_score)
+meteor_score = meteor.compute(predictions=outs, references=refs)
+print(meteor_score)
+rouge = load_metric('rouge')
+predictions = outs
+references = [u[0] for u in refs]
+rouge_score = rouge.compute(predictions=predictions,
+                        references=references)
+print(rouge_score)
+outs = [tokenizer.tokenize(u) for u in results['prds']]
+refs = [[tokenizer.tokenize(u)] for u in results['tars']]
+bleu_score = bleu.compute(predictions=outs, references=refs)
+print(bleu_score)
--- eval_1_2.py
+++ eval_1_2.py
@@ -16,7 +16,7 @@ from config import config
 from rank_bm25 import BM25Okapi
 from models.retrieval import TitleEncoder, PageRanker, SectionRanker
 
-with open('./data/test/dataset-aligned.pkl', 'rb') as f:
+with open('./data/test/dataset-aligned-para.pkl', 'rb') as f:
     data_test = pickle.load(f)
 srcs_ = []
 tars_ = []
@@ -214,130 +214,132 @@ def pipieline(path_from):
             src += '。'
         src_sts = src.split('。')
         tar_sts = tar.split('。')
-        if len(src_sts) == len(tar_sts):
-            srcs += src_sts
-            tars += tar_sts
-
-    for src, tar in zip(srcs, tars):
-        src_ = step1_tokenizer([src], return_tensors="pt", padding=True, truncation=True)
-        x_ids = src_['input_ids']
-        x_mask = src_['attention_mask']
-        x_indicator = torch.zeros_like(x_ids)
-        outputs = model_step1(x_ids, attention_mask=x_mask, existing_indicates=x_indicator)
-        logits = outputs.logits
-        pre_label_0 = np.argmax(logits.detach().cpu().numpy(), axis=2)
-        x_indicator = obtain_indicator(x_ids[0], pre_label_0[0])
-        x_indicator = torch.LongTensor(x_indicator).unsqueeze(0)
-        outputs = model_step1(x_ids, attention_mask=x_mask, existing_indicates=x_indicator)
-        logits = outputs.logits
-        pre_label_f = np.argmax(logits.detach().cpu().numpy(), axis=2)
-        step2_input = obtain_step2_input(pre_label_f[0], src, x_ids[0], step1_tokenizer)
-        querys = step2_input[0]
-        querys_ori = copy.copy(querys)
-        temp = []
-        for query in querys:
-            if query in mark_key_equal:
-                temp.append(mark_key_equal[query])
-            else:
-                url = 'https://api.ownthink.com/kg/ambiguous?mention=%s' % query
-                headers = {
-                    'User-Agent': 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
-                }
-                try_count = 0
-                while try_count < 3:
-                    try:
-                        r = requests.get(url, headers=headers, timeout=5)
-                        break
-                    except Exception as e:
-                        try_count += 1
-                        print("trying %d time" % try_count)
-                        wait_gap = 3
-                        time.sleep((try_count + np.random.rand()) * wait_gap)
-                rs = json.loads(r.text)
-                rs = rs['data']
-                name_set = set()
-                name_set.add(query)
-                for one in rs:
-                    name_set.add(re.sub(r'\[.*\]', '', one[0]))
-                name_list = list(name_set)
-                new_query = ' '.join(name_list)
-                if len(new_query) == 0:
-                    new_query = query
-                if query != new_query:
-                    print("%s->%s" % (query, new_query))
-                mark_key_equal[query] = new_query
-                temp.append(new_query)
-        querys = temp
-        contexts = step2_input[1]
-        infer_titles = step2_input[2]
-        key_pos = step2_input[3]
-        if len(querys) == 0:
-            continue
-        query_embedding = modelp.query_embeddings(querys, contexts)
-        dis_scores = modelp(query_embedding=query_embedding, candidates=infer_titles, is_infer=True)
-        rs_title = torch.topk(dis_scores, config.infer_title_select, dim=1)
-        scores_title = rs_title[0]
-        inds = rs_title[1].cpu().numpy()
-        infer_title_candidates_pured = []
-        infer_section_candidates_pured = []
-        mapping_title = np.zeros([len(querys), config.infer_title_select, config.infer_section_range])
-        src, src_tar, tar = pre_process_sentence(src, tar, querys_ori)
-        for query, bid in zip(querys, range(len(inds))):
+        batch_ans = []
+        for src, tar in zip(src_sts, tar_sts):
+            if len(src) == 0:
+                continue
+            src_ = step1_tokenizer([src], return_tensors="pt", padding=True, truncation=True)
+            x_ids = src_['input_ids']
+            x_mask = src_['attention_mask']
+            x_indicator = torch.zeros_like(x_ids)
+            outputs = model_step1(x_ids, attention_mask=x_mask, existing_indicates=x_indicator)
+            logits = outputs.logits
+            pre_label_0 = np.argmax(logits.detach().cpu().numpy(), axis=2)
+            x_indicator = obtain_indicator(x_ids[0], pre_label_0[0])
+            x_indicator = torch.LongTensor(x_indicator).unsqueeze(0)
+            outputs = model_step1(x_ids, attention_mask=x_mask, existing_indicates=x_indicator)
+            logits = outputs.logits
+            pre_label_f = np.argmax(logits.detach().cpu().numpy(), axis=2)
+            step2_input = obtain_step2_input(pre_label_f[0], src, x_ids[0], step1_tokenizer)
+            querys = step2_input[0]
+            querys_ori = copy.copy(querys)
             temp = []
-            temp2 = []
-            temp3 = []
-            count = 0
-            for nid, cid in enumerate(inds[bid]):
-                temp.append(infer_titles[bid][cid])
-                temp2 += title2sections[infer_titles[bid][cid]]
-                temp3 += [nid for x in title2sections[infer_titles[bid][cid]]]
-            temp2_id = []
-            for t_sec in temp2:
-                if t_sec in sec2id:
-                    temp2_id.append(sec2id[t_sec])
-            key_cut = jieba.lcut(query)
-            ls_scores = bm25_section.get_batch_scores(key_cut, temp2_id)
-            cindex = np.argsort(ls_scores)[::-1][0:config.infer_section_range]
-            temp2_pured = []
-            for oid, one in enumerate(cindex):
-                temp2_pured.append(temp2[one])
-                mapping_title[bid, temp3[one], oid] = 1
-            while len(temp2_pured) < config.infer_section_range:
-                temp2_pured.append('')
+            for query in querys:
+                if query in mark_key_equal:
+                    temp.append(mark_key_equal[query])
+                else:
+                    url = 'https://api.ownthink.com/kg/ambiguous?mention=%s' % query
+                    headers = {
+                        'User-Agent': 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
+                    }
+                    try_count = 0
+                    while try_count < 3:
+                        try:
+                            r = requests.get(url, headers=headers, timeout=5)
+                            break
+                        except Exception as e:
+                            try_count += 1
+                            print("trying %d time" % try_count)
+                            wait_gap = 3
+                            time.sleep((try_count + np.random.rand()) * wait_gap)
+                    rs = json.loads(r.text)
+                    rs = rs['data']
+                    name_set = set()
+                    name_set.add(query)
+                    for one in rs:
+                        name_set.add(re.sub(r'\[.*\]', '', one[0]))
+                    name_list = list(name_set)
+                    new_query = ' '.join(name_list)
+                    if len(new_query) == 0:
+                        new_query = query
+                    if query != new_query:
+                        print("%s->%s" % (query, new_query))
+                    mark_key_equal[query] = new_query
+                    temp.append(new_query)
+            querys = temp
+            contexts = step2_input[1]
+            infer_titles = step2_input[2]
+            key_pos = step2_input[3]
+            if len(querys) == 0:
+                continue
+            query_embedding = modelp.query_embeddings(querys, contexts)
+            dis_scores = modelp(query_embedding=query_embedding, candidates=infer_titles, is_infer=True)
+            rs_title = torch.topk(dis_scores, config.infer_title_select, dim=1)
+            scores_title = rs_title[0]
+            inds = rs_title[1].cpu().numpy()
+            infer_title_candidates_pured = []
+            infer_section_candidates_pured = []
+            mapping_title = np.zeros([len(querys), config.infer_title_select, config.infer_section_range])
+            src, src_tar, tar = pre_process_sentence(src, tar, querys_ori)
+            for query, bid in zip(querys, range(len(inds))):
+                temp = []
+                temp2 = []
+                temp3 = []
+                count = 0
+                for nid, cid in enumerate(inds[bid]):
+                    temp.append(infer_titles[bid][cid])
+                    temp2 += title2sections[infer_titles[bid][cid]]
+                    temp3 += [nid for x in title2sections[infer_titles[bid][cid]]]
+                temp2_id = []
+                for t_sec in temp2:
+                    if t_sec in sec2id:
+                        temp2_id.append(sec2id[t_sec])
+                key_cut = jieba.lcut(query)
+                ls_scores = bm25_section.get_batch_scores(key_cut, temp2_id)
+                cindex = np.argsort(ls_scores)[::-1][0:config.infer_section_range]
+                temp2_pured = []
+                for oid, one in enumerate(cindex):
+                    temp2_pured.append(temp2[one])
+                    mapping_title[bid, temp3[one], oid] = 1
+                while len(temp2_pured) < config.infer_section_range:
+                    temp2_pured.append('')
 
-            infer_title_candidates_pured.append(temp)
-            infer_section_candidates_pured.append(temp2_pured)
+                infer_title_candidates_pured.append(temp)
+                infer_section_candidates_pured.append(temp2_pured)
 
-        mapping = torch.FloatTensor(mapping_title).to(config.device)
-        scores_title = scores_title.unsqueeze(1)
-        scores_title = scores_title.matmul(mapping).squeeze(1)
-        rs_scores = models.infer(query_embedding, infer_section_candidates_pured)
-        scores = scores_title * rs_scores
-        rs2 = torch.topk(scores, config.infer_section_select, dim=1)
-        scores = rs2[0]
-        reference = []
-        inds_sec = rs2[1].cpu().numpy()
-        for bid in range(len(inds_sec)):
-            temp = [querys[bid]]
-            for indc in inds_sec[bid]:
-                temp.append(infer_section_candidates_pured[bid][indc][0:config.maxium_sec])
-            temp = ' [SEP] '.join(temp)
-            reference.append(temp[0:500])
-        record_scores.append(scores.detach().cpu().numpy())
-        record_references.append(reference)
-        inputs_ref = tokenizer(reference, return_tensors="pt", padding=True, truncation=True)
-        reference_ids = inputs_ref['input_ids'].to(config.device)
-        adj_matrix = get_decoder_att_map(tokenizer, '[SEP]', reference_ids, scores)
-        outputs_annotation = modele(input_ids=reference_ids, attention_adjust=adj_matrix)
-        hidden_annotation = outputs_annotation.decoder_hidden_states[:, 0:config.hidden_anno_len]
-        results, target_ids = restricted_decoding(querys_ori, [src], [src_tar], hidden_annotation, tokenizer, modeld)
-        results = [x.replace('（）', '') for x in results]
-        print(results[0])
-        results = [x.replace('$', '') for x in results]
-        # masks = torch.ones_like(targets)
-        # masks[torch.where(targets == 0)] = 0
-        eval_ans += results
+            mapping = torch.FloatTensor(mapping_title).to(config.device)
+            scores_title = scores_title.unsqueeze(1)
+            scores_title = scores_title.matmul(mapping).squeeze(1)
+            rs_scores = models.infer(query_embedding, infer_section_candidates_pured)
+            scores = scores_title * rs_scores
+            rs2 = torch.topk(scores, config.infer_section_select, dim=1)
+            scores = rs2[0]
+            reference = []
+            inds_sec = rs2[1].cpu().numpy()
+            for bid in range(len(inds_sec)):
+                temp = [querys[bid]]
+                for indc in inds_sec[bid]:
+                    temp.append(infer_section_candidates_pured[bid][indc][0:config.maxium_sec])
+                temp = ' [SEP] '.join(temp)
+                reference.append(temp[0:500])
+            record_scores.append(scores.detach().cpu().numpy())
+            record_references.append(reference)
+            inputs_ref = tokenizer(reference, return_tensors="pt", padding=True, truncation=True)
+            reference_ids = inputs_ref['input_ids'].to(config.device)
+            adj_matrix = get_decoder_att_map(tokenizer, '[SEP]', reference_ids, scores)
+            outputs_annotation = modele(input_ids=reference_ids, attention_adjust=adj_matrix)
+            hidden_annotation = outputs_annotation.decoder_hidden_states[:, 0:config.hidden_anno_len]
+            results, target_ids = restricted_decoding(querys_ori, [src], [src_tar], hidden_annotation, tokenizer, modeld)
+            results = [x.replace('（）', '') for x in results]
+            print(results[0])
+            results = [x.replace('$', '') for x in results]
+            # masks = torch.ones_like(targets)
+            # masks[torch.where(targets == 0)] = 0
+            batch_ans += results
         eval_gt += [tar]
+        section_rs = '。'.join(batch_ans)
+        section_rs += '。'
+        eval_ans += [section_rs]
     result_final = {'srcs': srcs, 'prds': eval_ans, 'tars': eval_gt, 'scores': record_scores,
                     'reference': record_references}
     with open('./data/test/my_results_sec.pkl', 'wb') as f:
