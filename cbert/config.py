5280305ead62ba20ed62e5e435a36f1574033f10
asbljy <asbljy@outlook.com>
2022-05-27 14:49:45 -0400

    update

--- cbert/config.py
+++ cbert/config.py
@@ -11,7 +11,7 @@ class Config(object):
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')   # 设备
 
         self.require_improvement = 1000                                 # 若超过1000batch效果还没提升，则提前结束训练
-        self.num_epochs = 10                                            # epoch数
+        self.num_epochs = 20                                            # epoch数
         self.batch_size = batch_size                                       # mini-batch大小
         self.pad_size = 512                                            # 每句话处理成的长度(短填长切)
         self.learning_rate = 5e-6                                      # 学习率
