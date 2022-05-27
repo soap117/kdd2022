4670a4059ec647f73d34d20d9799af0ec03d271f
asbljy <asbljy@outlook.com>
2022-05-27 16:19:55 -0400

    update

--- eval_1_2_sec.py
+++ eval_1_2_sec.py
@@ -135,11 +135,14 @@ def mark_sentence(input_list):
         else:
             region = (0, 0)
         if region[0] != 0 or region[1] != 0:
+            tar_sentence = tar_sentence[0:region[0]] + ' ${}$ '.format(key) + tar_sentence[region[1]:]
+            '''
             if region[1] < len(tar_sentence) and tar_sentence[region[1]] != '（' and region[1] + 1 < len(
                     tar_sentence) and tar_sentence[region[1] + 1] != '（' or region[1] == len(tar_sentence) or region[1]+1 == len(tar_sentence):
                 tar_sentence = tar_sentence[0:region[0]] + ' ${}$ （）'.format(key) + tar_sentence[region[1]:]
             else:
                 tar_sentence = tar_sentence[0:region[0]] + ' ${}$ '.format(key) + tar_sentence[region[1]:]
+            '''
         context_dic[context][0] = src_sentence
         context_dic[context][1] = tar_sentence
     order_context = []
