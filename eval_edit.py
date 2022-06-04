6e030445a698c297e877b9534eb2402c54046a05
asbljy <asbljy@outlook.com>
2022-06-04 14:50:09 -0400

    update

--- eval_edit.py
+++ eval_edit.py
@@ -432,7 +432,7 @@ def pipieline(path_from):
 
     result_final = {'srcs': srcs, 'prds': eval_ans, 'tars': eval_gt, 'scores': record_scores,
                     'reference': record_references}
-    with open('./data/test/my_results_sec.pkl', 'wb') as f:
+    with open('./data/test/my_results_edit_sec.pkl', 'wb') as f:
         pickle.dump(result_final, f)
 
 
