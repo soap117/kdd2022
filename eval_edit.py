0add577f8b5189df92fccb565b4d78daf6a59368
asbljy <asbljy@outlook.com>
2022-06-04 14:49:12 -0400

    update

--- eval_edit.py
+++ eval_edit.py
@@ -24,7 +24,7 @@ tars_ = []
 for point in data_test:
     srcs_.append(point[0])
     tars_.append(point[1])
-save_data = torch.load('./results/' + config.data_file.replace('.pkl', '_models_full.pkl').replace('data/', ''))
+save_data = torch.load('./results/' + config.data_file.replace('.pkl', '_models_edit.pkl').replace('data/', ''))
 save_step1_data = torch.load('./cbert/cache/' + 'best_save.data')
 
 
