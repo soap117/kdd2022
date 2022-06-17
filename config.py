98ca27b4bee5482e8dbd72c406d4263b1f06cb7d
asbljy <asbljy@outlook.com>
2022-06-17 09:54:15 -0400

    update

--- config.py
+++ config.py
@@ -32,8 +32,8 @@ class Config(object):
         self.infer_section_range = 10
         self.infer_section_select = 3
         self.maxium_sec = 512
-        self.hidden_anno_len = 5
-        self.hidden_anno_len_rnn = 5
+        self.hidden_anno_len = 10
+        self.hidden_anno_len_rnn = 10
         self.multi_gpu = False
         self.full_gpu_id = 0
         self.max_query = 12
