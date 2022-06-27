42c1bfb91bc9969c0fa9f6e2254c5fddc0ec9f2c
asbljy <asbljy@outlook.com>
2022-06-27 12:17:28 -0400

    update

--- EditNTS/editnts.py
+++ EditNTS/editnts.py
@@ -1325,6 +1325,249 @@ class EditDecoderRNNDualSICRoss(nn.Module):
         return Variable(weight.new(self.n_layers, bsz, self.hidden_size).zero_()), \
                Variable(weight.new(self.n_layers, bsz, self.hidden_size).zero_())
 
+class EditDecoderRNNDualCRoss(nn.Module):
+    def __init__(self, vocab_size, embedding_dim, hidden_size, n_layers=1, embedding=None):
+        super(EditDecoderRNNDualCRoss, self).__init__()
+        self.hidden_size = hidden_size
+        self.embedding_dim = embedding_dim
+        self.vocab_size = vocab_size
+        self.n_layers = n_layers
+
+        if embedding is None:
+            self.embedding = nn.Embedding(vocab_size, embedding_dim)
+        else:
+            self.embedding = embedding
+        self.rnn_edits = nn.LSTM(embedding_dim, hidden_size, num_layers=n_layers, batch_first=True)
+        self.rnn_actions = nn.LSTM(embedding_dim, hidden_size, num_layers=n_layers, batch_first=True)
+        self.rnn_words = nn.LSTM(embedding_dim, hidden_size, num_layers=n_layers, batch_first=True)
+        self.attn_Projection_org = nn.Linear(hidden_size, hidden_size, bias=False)
+        # self.attn_Projection_scpn = nn.Linear(hidden_size, hidden_size, bias=False) #hard attention here
+
+
+        self.attn_MLP_action = nn.Sequential(nn.Linear(hidden_size * 5, embedding_dim),
+                                          nn.Tanh())
+        self.attn_MLP_edit = nn.Sequential(nn.Linear(hidden_size * 5, embedding_dim),
+                                      nn.Tanh())
+        self.out_edit = nn.Linear(embedding_dim, self.vocab_size)
+        self.out_edit.weight.data = self.embedding.weight.data[:self.vocab_size]
+
+        self.out_action = nn.Linear(embedding_dim, 7)
+        self.out_action.weight.data = self.embedding.weight.data[:7]
+
+    def execute(self, symbol, input, lm_state):
+        """
+        :param symbol: token_id for predicted edit action (in teacher forcing mode, give the true one)
+        :param input: the word_id being editted currently
+        :param lm_state: last lstm state
+        :return:
+        """
+        # predicted_symbol = KEEP -> feed input to RNN_LM
+        # predicted_symbol = DEL -> do nothing, return current lm_state
+        # predicted_symbol = new word -> feed that word to RNN_LM
+        is_keep = torch.eq(symbol, data.KEEP_ID)
+        is_del = torch.eq(symbol, data.DEL_ID)
+        if is_del:
+            return lm_state
+        elif is_keep: # return lstm with kept word learned in lstm
+            _, new_lm_state = self.rnn_words(self.embedding(input.view(-1, 1)), lm_state)
+        else: #consider as insert here
+            # print(symbol.item())
+            input = self.embedding(symbol.view(-1,1))
+            _, new_lm_state = self.rnn_words(input,lm_state)
+        return new_lm_state
+
+    def execute_batch(self, batch_symbol, batch_input, batch_lm_state):
+        batch_h = batch_lm_state[0]
+        batch_c = batch_lm_state[1]
+
+        bsz = batch_symbol.size(0)
+        unbind_new_h = []
+        unbind_new_c = []
+
+        # unbind all batch inputs
+        unbind_symbol = torch.unbind(batch_symbol,dim=0)
+        unbind_input = torch.unbind(batch_input,dim=0)
+        unbind_h = torch.unbind(batch_h,dim=1)
+        unbind_c = torch.unbind(batch_c,dim=1)
+        for i in range(bsz):
+            elem=self.execute(unbind_symbol[i], unbind_input[i], (unbind_h[i].view(1,1,-1), unbind_c[i].view(1,1,-1)))
+            unbind_new_h.append(elem[0])
+            unbind_new_c.append(elem[1])
+        new_batch_lm_h = torch.cat(unbind_new_h,dim=1)
+        new_batch_lm_c = torch.cat(unbind_new_c,dim=1)
+        return (new_batch_lm_h,new_batch_lm_c)
+
+
+    def forward(self, input_edits, hidden_org,encoder_outputs_org, org_ids, simp_sent,teacher_forcing_ratio=1.):
+        #input_edits and simp_sent need to be padded with START
+        bsz, nsteps = input_edits.size()
+
+        # revisit each word and then decide the action, for each action, do the modification and calculate rouge difference
+        use_teacher_forcing = True if random.random() < teacher_forcing_ratio else False
+        decoder_edit_out = []
+        decoder_action_out = []
+        counter_for_keep_del = np.zeros(bsz, dtype=int)
+        counter_for_keep_ins =np.zeros(bsz, dtype=int)
+        # decoder in the training:
+        input_actions = torch.zeros_like(input_edits) + INSERT_ID
+        input_actions = torch.where((input_edits!=0)&(input_edits!=1)&(input_edits!=2)&(input_edits!=3)&(input_edits!=4)&(input_edits!=5), input_actions, input_edits)
+        if use_teacher_forcing:
+            embedded_edits = self.embedding(input_edits)
+            output_edits, hidden_edits = self.rnn_edits(embedded_edits, hidden_org)
+
+            embedded_actions = self.embedding(input_actions)
+            output_actions, hidden_actions = self.rnn_actions(embedded_actions, hidden_org)
+
+            embedded_words = self.embedding(simp_sent)
+            output_words, hidden_words = self.rnn_words(embedded_words, hidden_org)
+
+
+            key_org_edits = self.attn_Projection_org(output_edits)  # bsz x nsteps x nhid MIGHT USE WORD HERE
+            logits_org = torch.bmm(key_org_edits, encoder_outputs_org.transpose(1, 2))  # bsz x nsteps x encsteps
+            attn_weights_org_edits = F.softmax(logits_org, dim=-1)  # bsz x nsteps x encsteps
+            attn_applied_org_edits = torch.bmm(attn_weights_org_edits, encoder_outputs_org)  # bsz x nsteps x nhid
+
+            key_org_actions = self.attn_Projection_org(output_actions)  # bsz x nsteps x nhid MIGHT USE WORD HERE
+            logits_org = torch.bmm(key_org_actions, encoder_outputs_org.transpose(1, 2))  # bsz x nsteps x encsteps
+            attn_weights_org_actions = F.softmax(logits_org, dim=-1)  # bsz x nsteps x encsteps
+            attn_applied_org_actions = torch.bmm(attn_weights_org_actions, encoder_outputs_org)  # bsz x nsteps x nhid
+
+            for t in range(nsteps-1):
+                # print(t)
+                decoder_output_edit_t = output_edits[:, t:t + 1, :]
+                attn_applied_org_edit_t = attn_applied_org_edits[:, t:t + 1, :]
+                decoder_output_action_t = output_actions[:, t:t + 1, :]
+                attn_applied_org_action_t = attn_applied_org_actions[:, t:t + 1, :]
+
+                ## find current word
+                inds = torch.LongTensor(counter_for_keep_del)
+                dummy = inds.view(-1, 1, 1)
+                dummy = dummy.expand(dummy.size(0), dummy.size(1), encoder_outputs_org.size(2)).cuda()
+                c = encoder_outputs_org.gather(1, dummy)
+
+                inds = torch.LongTensor(counter_for_keep_ins)
+                dummy = inds.view(-1, 1, 1)
+                dummy = dummy.expand(dummy.size(0), dummy.size(1), output_words.size(2)).cuda()
+                c_word = output_words.gather(1, dummy)
+
+                output_edit_t = torch.cat((decoder_output_edit_t, decoder_output_action_t, attn_applied_org_edit_t, c,c_word),
+                                     2)  # bsz*nsteps x nhid*2
+                output_edit_t = self.attn_MLP_action(output_edit_t)
+                output_edit_t = F.log_softmax(self.out_edit(output_edit_t), dim=-1)
+                decoder_edit_out.append(output_edit_t)
+
+                output_action_t = torch.cat((decoder_output_action_t, decoder_output_edit_t, attn_applied_org_action_t, c, c_word),
+                                          2)  # bsz*nsteps x nhid*2
+                output_action_t = self.attn_MLP_edit(output_action_t)
+                output_action_t = F.log_softmax(self.out_action(output_action_t), dim=-1)
+                decoder_action_out.append(output_action_t)
+
+                # interpreter's output from lm
+                gold_action = input_edits[:, t + 1].data.cpu().numpy()  # might need to realign here because start added
+                counter_for_keep_del = [i[0] + 1 if i[1] == 2 or i[1] == 3  else i[0]
+                                        for i in zip(counter_for_keep_del, gold_action)]
+                counter_for_keep_ins = [i[0] + 1 if i[1] != DEL_ID and i[1] != STOP_ID and i[1] != PAD_ID else i[0]
+                                        for i in zip(counter_for_keep_ins, gold_action)]
+
+                check1 = sum([x >= org_ids.size(1) for x in counter_for_keep_del])
+                check2 = sum([x >= simp_sent.size(1) for x in counter_for_keep_ins])
+                if check1 or check2:
+                    # print(org_ids.size(1))
+                    # print(counter_for_keep_del)
+                    break
+
+
+        else: # no teacher forcing
+            decoder_input_edit = input_edits[:, :1]
+            decoder_input_action = input_actions[:, :1]
+            decoder_input_word = simp_sent[:,:1]
+            t, tt = 0, max(MAX_LEN,input_edits.size(1)-1)
+
+            # initialize
+            embedded_edits = self.embedding(decoder_input_edit)
+            output_edits, hidden_edits = self.rnn_edits(embedded_edits, hidden_org)
+
+            embedded_actions = self.embedding(decoder_input_action)
+            output_actions, hidden_actions = self.rnn_actions(embedded_actions, hidden_org)
+
+            embedded_words = self.embedding(decoder_input_word)
+            output_words, hidden_words = self.rnn_words(embedded_words, hidden_org)
+            #
+            # # give previous word from tgt simp_sent
+            # inds = torch.LongTensor(counter_for_keep_ins)
+            # dummy = inds.view(-1, 1, 1)
+            # dummy = dummy.expand(dummy.size(0), dummy.size(1), output_words.size(2)).cuda()
+            # c_word = output_words.gather(1, dummy)
+
+            while t < tt:
+                if t>0:
+                    embedded_edits = self.embedding(decoder_input_edit)
+                    output_edits, hidden_edits = self.rnn_edits(embedded_edits, hidden_edits)
+                    embedded_actions = self.embedding(decoder_input_edit)
+                    output_actions, hidden_actions = self.rnn_actions(embedded_actions, hidden_actions)
+
+                key_org_edits = self.attn_Projection_org(output_edits)  # bsz x nsteps x nhid
+                logits_org = torch.bmm(key_org_edits, encoder_outputs_org.transpose(1, 2))  # bsz x nsteps x encsteps
+                attn_weights_org_edit_t = F.softmax(logits_org, dim=-1)  # bsz x nsteps x encsteps
+                attn_applied_org_edit_t = torch.bmm(attn_weights_org_edit_t, encoder_outputs_org)  # bsz x nsteps x nhid
+
+                key_org_actions = self.attn_Projection_org(output_actions)  # bsz x nsteps x nhid
+                logits_org = torch.bmm(key_org_actions, encoder_outputs_org.transpose(1, 2))  # bsz x nsteps x encsteps
+                attn_weights_org_action_t = F.softmax(logits_org, dim=-1)  # bsz x nsteps x encsteps
+                attn_applied_org_action_t = torch.bmm(attn_weights_org_action_t, encoder_outputs_org)
+
+                ## find current word
+                inds = torch.LongTensor(counter_for_keep_del)
+                dummy = inds.view(-1, 1, 1)
+                dummy = dummy.expand(dummy.size(0), dummy.size(1), encoder_outputs_org.size(2)).cuda()
+                c = encoder_outputs_org.gather(1, dummy)
+
+                output_action_t = torch.cat((output_actions, output_edits, attn_applied_org_action_t, c, hidden_words[0]),
+                                     2)  # bsz*nsteps x nhid*2
+                output_action_t = self.attn_MLP_action(output_action_t)
+                output_action_t = F.log_softmax(self.out_action(output_action_t), dim=-1)
+
+                decoder_action_out.append(output_action_t)
+                decoder_input_action = torch.argmax(output_action_t, dim=2)
+
+                output_edit_t = torch.cat((output_edits, output_actions, attn_applied_org_edit_t, c, hidden_words[0]),
+                                            2)  # bsz*nsteps x nhid*2
+                output_edit_t = self.attn_MLP_edit(output_edit_t)
+                output_edit_t = F.log_softmax(self.out_edit(output_edit_t), dim=-1)
+
+                decoder_edit_out.append(output_edit_t)
+                decoder_input_edit = torch.argmax(output_edit_t, dim=2)
+
+
+                # gold_action = input[:, t + 1].vocab_data.cpu().numpy()  # might need to realign here because start added
+                pred_action = torch.argmax(output_action_t,dim=2)
+                pred_edit = torch.argmax(output_edit_t, dim=2)
+                pred_edit = torch.where(pred_action != INSERT_ID,  pred_action,
+                                                 pred_edit)
+
+                counter_for_keep_del = [i[0] + 1 if i[1] == 2 or i[1] == 3 or i[1] == 5 else i[0]
+                                        for i in zip(counter_for_keep_del, pred_action)]
+
+                # update rnn_words
+                # find previous generated word
+                # give previous word from tgt simp_sent
+                dummy_2 = inds.view(-1, 1).cuda()
+                org_t = org_ids.gather(1, dummy_2)
+                hidden_words = self.execute_batch(pred_edit, org_t, hidden_words)  # we give the editted subsequence
+                # hidden_words = self.execute_batch(pred_action, org_t, hidden_org)  #here we only give the word
+
+                t += 1
+                check = sum([x >= org_ids.size(1) for x in counter_for_keep_del])
+                if check:
+                    break
+        decoder_action_out = torch.cat(decoder_action_out, dim=1)
+        decoder_edit_out = torch.cat(decoder_edit_out, dim=1)
+        return (decoder_action_out, decoder_edit_out), hidden_edits
+
+    def initHidden(self, bsz):
+        weight = next(self.parameters()).data
+        return Variable(weight.new(self.n_layers, bsz, self.hidden_size).zero_()), \
+               Variable(weight.new(self.n_layers, bsz, self.hidden_size).zero_())
 
 class EditNTSDualSI(nn.Module):
     def __init__(self, config, n_layers=2):
