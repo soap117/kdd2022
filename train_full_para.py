c30239ec14c0ec19c54aa966ae842ba93067cdf3
asbljy <asbljy@outlook.com>
2022-07-11 00:04:14 -0400

    update

--- train_full_para.py
+++ train_full_para.py
@@ -90,11 +90,13 @@ def build(config):
     modele.load_state_dict(save_data['model'], strict=False)
     print('Load pretrained E')
     from models.modeling_bart_ex import BartModel, BartLearnedPositionalEmbedding
-    pos_embed = BartLearnedPositionalEmbedding(1024, 768)
+    pos_embed_encoder = BartLearnedPositionalEmbedding(1024, 768)
+    pos_embed_decoder = BartLearnedPositionalEmbedding(1024, 768)
     config.tokenizer.model_max_length = 1024
     tokenizer.model_max_length = 1024
     modeld = config.modeld_sen.from_pretrained(config.bert_model)
-    modeld.model.embed_positions = pos_embed
+    modeld.model.encoder.embed_positions = pos_embed_encoder
+    modeld.model.decoder.embed_positions = pos_embed_decoder
     modeld.cuda()
     modelp.to("cuda:1")
     models.to("cuda:1")
