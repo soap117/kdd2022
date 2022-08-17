923e9668782006606daa21d88c256709c706ff0f
asbljy <asbljy@outlook.com>
2022-08-17 12:12:23 -0400

    update

--- train_full.py
+++ train_full.py
@@ -55,7 +55,7 @@ def build(config):
     corpus = titles
     tokenized_corpus = [jieba.lcut(doc) for doc in corpus]
     bm25_title = BM25Okapi(tokenized_corpus)
-    debug_flag = False
+    debug_flag = True
     if not debug_flag and os.path.exists(config.data_file_old.replace('.pkl', '_train_dataset_{}.pkl'.format(config.hidden_anno_len))):
         train_dataset = torch.load(config.data_file_old.replace('.pkl', '_train_dataset_{}.pkl'.format(config.hidden_anno_len)))
         valid_dataset = torch.load(config.data_file_old.replace('.pkl', '_valid_dataset_{}.pkl'.format(config.hidden_anno_len)))
